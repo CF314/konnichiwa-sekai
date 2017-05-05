d55c1c68aee6d2be292aec6fb04551dacd75d968
CF314 <CF314@users.noreply.github.com>
2017-05-05 21:00:46 +0200

    Added first build of the program
    Shows "Hello World!" and allows you to greet back.
    Known errors:
    *Image doesn't show
    *Not resizing properly

---
+++ KonnichiwaSekai/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KonnichiwaSekai
+{
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// This programming behind this programs main window.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnResponseClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thank you!", "こんにちは世界");
+        }
+    }
+}
